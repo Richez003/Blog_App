0dedae1dda1fe909af336dce33171bd603534127
richemonday@gmail.com <richemonday@gmail.com>
2022-09-02 15:59:18 +0100

    fixing rubocop error

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,11 +1,8 @@
 class Comment < ApplicationRecord
-    belongs_to :user
-    belongs_to :post
-  
-    
-    def comments_posts_counter
-     post.update(comments_counter: post.comments.count)
+  belongs_to :user
+  belongs_to :post
 
-
-end
+  def comments_posts_counter
+    post.update(comments_counter: post.comments.count)
+  end
 end
