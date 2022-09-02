c600c585d7ad22459a12937262982a5358d3ea30
richemonday@gmail.com <richemonday@gmail.com>
2022-09-02 15:43:59 +0100

    created models with the relationshipd

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,11 @@
 class Comment < ApplicationRecord
+    belongs_to :user
+    belongs_to :post
+  
+    
+    def comments_posts_counter
+     post.update(comments_counter: post.comments.count)
+
+
+end
 end
