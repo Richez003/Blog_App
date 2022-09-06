83f94f0e05c34d354c826d226142d48b68997e11
richemonday@gmail.com <richemonday@gmail.com>
2022-09-06 14:05:24 +0100

    fixed rubocop errors

--- spec/request/users_controller_spec.rb
+++ spec/request/users_controller_spec.rb
@@ -1,22 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe 'Users', type: :request do
-        context 'GET #index' do
-        before(:each) { get users_path } # get(:index)
-        it 'it renders succesfully' do
-          expect(response).to have_http_status(:ok)
-        end
+  context 'GET #index' do
+    before(:each) { get users_path } # get(:index)
+    it 'it renders succesfully' do
+      expect(response).to have_http_status(:ok)
+    end
 
-        it "it shows the 'index' template correctly" do
-          expect(response).to render_template('index')
-        end
+    it "it shows the 'index' template correctly" do
+      expect(response).to render_template('index')
+    end
 
-        it 'it shows "Here is a list of users" correctly' do
-          expect(response.body).to include('Here is a list of users')
-        end
-   end
+    it 'it shows "Here is a list of users" correctly' do
+      expect(response.body).to include('Here is a list of users')
+    end
+  end
 
-   context  'GET #show' do
+  context 'GET #show' do
     before(:each) { get '/users/2' } # gets(:show, params: { id: 2 })
     it 'it renders succesfully' do
       expect(response).to have_http_status(:ok)
@@ -27,5 +27,5 @@ RSpec.describe 'Users', type: :request do
     it 'it shows Here the details of a user: Name, Photo, Bio, and Posts" correctly' do
       expect(response.body).to include('Here the details of a user: Name, Photo, Bio, and Posts')
     end
-   end
   end
+end
